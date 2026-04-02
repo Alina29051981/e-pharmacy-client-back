1533be612f9a4a0ef3ddd1dffbbe9c8c6b0480ea
Alina Kuchma <ostaliva@ukr.net>
2026-04-02 18:05:23 +0300

    Added validation for auth

---
+++ src/validations/authValidation.js
@@ -0,0 +1,38 @@
+import { Joi, Segments } from "celebrate";
+
+export const registerUserSchema = {
+    [Segments.BODY]: Joi.object({
+        name: Joi.string().min(2).max(50).required(),
+
+        email: Joi.string().email().required(),
+
+        phone: Joi.string()
+            .pattern(/^\+?[0-9]{10,15}$/)
+            .optional(),
+
+        password: Joi.string()
+            .min(8)
+            .pattern(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d@$!%*?&]+$/)
+            .required(),
+    }),
+};
+
+export const loginUserSchema = {
+    [Segments.BODY]: Joi.object({
+        email: Joi.string().email().required(),
+        password: Joi.string().required(),
+    }),
+};
+
+export const requestResetEmailSchema = {
+    [Segments.BODY]: Joi.object({
+        email: Joi.string().email().required(),
+    }),
+};
+
+export const resetPasswordSchema = {
+    [Segments.BODY]: Joi.object({
+        password: Joi.string().min(8).required(),
+        token: Joi.string().required(),
+    }),
+};
