8fcb3683658cf0ad6c59c9e160cd3c31a915baeb
Alina Kuchma <ostaliva@ukr.net>
2026-04-05 11:12:18 +0300

    Added errorMiddleware file

---
+++ src/middleware/errorMiddleware.js
@@ -0,0 +1,14 @@
+// src/middleware/errorMiddleware.js
+
+export const errorHandler = (err, req, res, next) => {
+    console.error(err);
+
+    const status = err.status || 500;
+
+    res.status(status).json({
+        status: "error",
+        code: status,
+        message: err.message || "Server error",
+        ...(process.env.NODE_ENV !== "production" && { stack: err.stack }),
+    });
+};
