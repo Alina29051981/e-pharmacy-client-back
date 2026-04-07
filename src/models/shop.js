6bf1d9516e4f45a82b2cbc387928914a56f637f7
Alina Kuchma <ostaliva@ukr.net>
2026-04-07 23:52:13 +0300

    add shop model

---
+++ src/models/shop.js
@@ -0,0 +1,40 @@
+// src/models/shop.js
+
+import mongoose from "mongoose";
+
+const shopSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+
+        owner: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+        },
+
+        email: {
+            type: String,
+            required: true,
+            lowercase: true,
+        },
+
+        phone: {
+            type: String,
+        },
+
+        address: {
+            type: String,
+        },
+
+        logo: {
+            type: String, // URL або шлях до файлу
+        },
+    },
+    { timestamps: true },
+);
+
+export default mongoose.model("Shop", shopSchema);
